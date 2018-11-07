2188d7c7196c21447494437c40584ba820e6cac5
fmsunyh <fmsunyh@gmail.com>
2018-11-07 10:28:21 +0800

    Add pascal_voc.py

---
+++ core/preprocessing/__init__.py
@@ -0,0 +1,8 @@
+# -----------------------------------------------------
+# -*- coding: utf-8 -*-
+# @Time    : 10/31/2018 10:43 AM
+# @Author  : sunyonghai
+# @Software: ZJ_AI
+# -----------------------------------------------------
+
+from .pascal_voc import PascalVocIterator
